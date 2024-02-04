b6637a9af04e3d59f6b45f019089e4f894f48aaa
Ezequiel Cardozo Soto <ezequielcardozosoto@gmail.com>
2024-02-04 07:00:29 -0300

    TerceraPreentregaJS-2

--- app.js
+++ app.js
@@ -5,53 +5,86 @@
 
 
 
+/* const carrito = [ 
+    { nombre: ' Johnnie Walker Blue Label', bebidaTipo: 'Whisky', precio: 310000, cantidad: 0, disponible: true},
+    { nombre: ' Johnnie Walker Green Label', bebidaTipo: 'Whisky', precio: 210000,cantidad: 0, disponible: true},
+    { nombre: ' Martin Miller s London Gin', bebidaTipo: 'Gin', precio: 55000, cantidad: 0, disponible: false},
+    { nombre: ' Gin Bulldog London Dry 700 mL', bebidaTipo: 'Gin', precio: 40000, cantidad: 0, disponible: false},
+    { nombre: ' Aperitivo Fernet Branca 750cc', bebidaTipo: 'Aperitivo', precio: 8500,cantidad: 0, disponible: true}
+]; */
 
 
+// utilizo una clase constructora para agregar bebidas
+class Bebida{
+    constructor(nombre, bebidaTipo, precio, cantidad){
+        this.nombre = nombre;
+        this.bebidaTipo = bebidaTipo;
+        this.precio = parseFloat(precio);
+        this.cantidad = cantidad;
+        this.disponible = true;
 
 
+    }
+    sumarIva(){
+        return this.precio * 1.21;
+    }
+    vender(){
+        return this.disponible = false;
+    
+    }
+    precioVip(){
+        return this.precio * 1.21 * 0.25;
+    }
 
+}
 
+const ListaBebidas = [];
+let entrada;
 
-const carrito = [ 
-    { nombre: ' Johnnie Walker Blue Label', bebidaTipo: 'Whisky', precio: 310000},
-    { nombre: ' Johnnie Walker Green Label', bebidaTipo: 'Whisky', precio: 210000},
-    { nombre: ' Martin Miller s London Gin', bebidaTipo: 'Gin', precio: 55000},
-    { nombre: ' Gin Bulldog London Dry 700 mL', bebidaTipo: 'Gin', precio: 40000},
-    { nombre: ' Aperitivo Fernet Branca 750cc', bebidaTipo: 'Aperitivo', precio: 8500}
-];
+do{
+    let entrada = prompt(`ingrese el nombre del producto o end para finalizar de agregar`);
+    if(entrada ===`end` || entrada ===` END` || entrada === `End`){
+        break
+    }else{
+        nombreBebida = entrada;
+        let TipoBebida = prompt (`ingrese el tipo de bebida`);
+        let precioBebida = prompt(`Ingrese el precio de la bebida`);
+        let cantidad = parseInt(prompt(`ingrese la cantidad del producto deseado`));
+        ListaBebidas.push(new Bebida(nombreBebida, TipoBebida, precioBebida, cantidad   ))
+    }
+}
+while(entrada != `end` || entrada != `END` || entrada != `End`);
 
 //subo un producto más a la lista
 
-carrito.push({ nombre: 'Aperitivo Campari 750cc', bebidaTipo: 'Aperitivo', precio: 5500});
-console.log(carrito);
+ListaBebidas.push({ nombre: 'Aperitivo Campari 750cc', bebidaTipo: 'Aperitivo', precio: 5500});
+
+console.log(ListaBebidas);
+
 
 //busco un precio de bebida entre los 5000 y 50000 pesos
-const encontrado = carrito.filter( (item) => (item.precio <= 50000 && item.precio >= 5000));
+const encontrado = ListaBebidas.filter( (item) => (item.precio <= 50000 && item.precio >= 5000));
 console.log(encontrado);
 
+
 // aqui busco si tengo una bebida por la funcion some
-const encontrado2 = carrito.some((item) => item.nombre == ' Johnnie Walker Blue Label'); 
+const encontrado2 = ListaBebidas.some((item) => item.nombre == 'Aperitivo Campari 750cc'); 
 console.log(encontrado2);
 
 
-
-
 //los precios suben por inflacion 25% en enero
-const subaPrecios = carrito.map( (item )=> item.precio * 1.25);
+const subaPrecios = ListaBebidas.map( (item )=> item.precio * 1.25);
 
 console.log(subaPrecios);
 console.table(subaPrecios);
 
-
 // o asi para que salga tambien el nombre de la bebida y el precio
-
-const preciActualizado = carrito.map((item) => {
+const preciActualizado = ListaBebidas.map((item) => {
     return {
         nombre: item.nombre,
         precio: item.precio * 1.25
     }
-})
-
+});
 
 console.log(preciActualizado);
 console.table(preciActualizado);
@@ -59,7 +92,7 @@ console.table(preciActualizado);
 
 
 // se ordena de menor a mayor los precios
-carrito.sort( (a, b) => {
+ListaBebidas.sort( (a, b) => {
     if( a.precio > b.precio){
         return 1
     }
@@ -70,4 +103,7 @@ carrito.sort( (a, b) => {
     return 0
 });
 
-console.log(carrito);
+console.log(ListaBebidas);
+
+
+
